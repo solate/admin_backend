162bf27082efcb485333d3d139a958a5bb456cd9
solate <349370626@qq.com>
2025-02-12 10:20:50 +0800

    feat: 获取验证码

--- app/admin/internal/logic/auth/get_captcha_logic.go
+++ app/admin/internal/logic/auth/get_captcha_logic.go
@@ -2,9 +2,11 @@ package auth
 
 import (
 	"context"
+	"fmt"
 
 	"admin_backend/app/admin/internal/svc"
 	"admin_backend/app/admin/internal/types"
+	"admin_backend/pkg/common/xerr"
 
 	"github.com/zeromicro/go-zero/core/logx"
 )
@@ -25,7 +27,19 @@ func NewGetCaptchaLogic(ctx context.Context, svcCtx *svc.ServiceContext) *GetCap
 }
 
 func (l *GetCaptchaLogic) GetCaptcha() (resp *types.CaptchaResp, err error) {
-	// todo: add your logic here and delete this line
+	fmt.Println("============1111=====")
 
-	return
+	// 生成验证码
+	id, b64s, _, err := l.svcCtx.CaptchaManager.Generate()
+	if err != nil {
+		l.Error("GetCaptcha Generate err:", err.Error())
+		return nil, xerr.NewErrCode(xerr.ServerError)
+	}
+
+	fmt.Println("=================", id, b64s)
+
+	return &types.CaptchaResp{
+		CaptchaId:  id,
+		CaptchaUrl: b64s,
+	}, nil
 }
